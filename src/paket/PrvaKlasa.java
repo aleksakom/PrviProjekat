4edfc32a6209c65c581bb06db16449cb4c25c8c4
aleksakomosar <aleksakomosar@gmail.com>
2023-09-14 17:35:22 +0200

    Initial commit

---
+++ src/paket/PrvaKlasa.java
@@ -0,0 +1,15 @@
+package paket;
+
+public class PrvaKlasa {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//prvi komentar
+		
+		
+		/*viselinijski
+		 * komentar
+		 */
+	}
+
+}
